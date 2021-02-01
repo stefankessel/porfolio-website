e9534fa2b64c9f7177eb3eb64ce753109a5e1962
Stefan Kessel <kessel.stefan1@gmail.com>
2021-02-01 12:03:35 +0100

    fixed?

--- server.js
+++ server.js
@@ -40,10 +40,10 @@ app.use('/api/download', downloadRoutes);
 if(process.env.NODE_ENV === 'production'){
     // serve routes defined in express or react
     //app.use(express.static('client/build'))
+    const path = require('path');
     app.use(express.static(path.resolve(__dirname, "../client/build")));
 
     // if route is not found, serve index.html
-    const path = require('path');
 
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
